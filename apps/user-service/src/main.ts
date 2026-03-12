d534b386a162ec08ed92cb1aa2492137923673e3
mdsharif <srka780@gmail.com>
2026-03-13 05:06:48 +0600

    include user-service

---
+++ apps/user-service/src/main.ts
@@ -0,0 +1,12 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import 'dotenv/config';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const port: number = Number(process.env.PORT ?? 3000);
+  await app.listen(port);
+  console.log('main service running on port', port);
+}
+void bootstrap();
