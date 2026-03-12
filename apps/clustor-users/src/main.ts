ae1497fecafc16ea8634ba00ee503434c315ba83
mdsharif <srka780@gmail.com>
2026-03-13 05:07:19 +0600

    set new service

--- apps/clustor-users/src/main.ts
+++ apps/clustor-users/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import 'dotenv/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const port: number = Number(process.env.PORT ?? 3000);
+  await app.listen(port);
+  console.log('main service running on port', port);
 }
-bootstrap();
+void bootstrap();
